0975251cc69d04b1034c1e8c1163d00f6069e387
ghidora77 <ozzycampos@yahoo.com>
2022-01-03 21:06:55 -0500

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,7 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'LozAnalytics', // Usually your GitHub org/user name.
   projectName: 'remote-sensing', // Usually your repo name.
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
-      crossorigin: 'anonymous',
-    },
-  ],
+
 
   themeConfig: {
     navbar: {
