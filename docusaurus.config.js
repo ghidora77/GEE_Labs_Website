9900b572fd5124fe3ac1d55464a3392f3e9ecb42
ghidora77 <ozzycampos@yahoo.com>
2022-01-03 20:57:31 -0500

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,32 +4,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const math = require('remark-math');
 const katex = require('rehype-katex');
 
-module.exports = {
-  title: 'Docusaurus',
-  tagline: 'Build optimized websites quickly, focus on your content',
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          path: 'docs',
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-        },
-      },
-    ],
-  ],
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css',
-      integrity:
-        'sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc',
-      crossorigin: 'anonymous',
-    },
-  ],
-};
-
-
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'Remote Sensing Documentation',
@@ -41,6 +15,16 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'LozAnalytics', // Usually your GitHub org/user name.
   projectName: 'remote-sensing', // Usually your repo name.
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
+      crossorigin: 'anonymous',
+    },
+  ],
+
   themeConfig: {
     navbar: {
       title: 'Remote Sensing',
@@ -100,6 +84,8 @@ module.exports = {
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
+          remarkPlugins: [math],
+          rehypePlugins: [katex]
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
